c4b867763380382a679d2db44ff7740cc751e64e
Arizapt <abriaz.putra3@gmail.com>
2024-05-23 22:49:25 +0700

    pH.py

--- pH.py
+++ pH.py
@@ -3,35 +3,105 @@ import streamlit as st
 def show_splash_screen():
     st.title('Penentuan Nilai pH')
     st.markdown("---")
-    st.write("Aplikasi ini akan membantu Anda menentukan sifat asam basa berdasarkan nilai pH yang dimasukkan.")
+    st.write("Aplikasi ini akan membantu Anda menentukan sifat asam basa berdasarkan nama sampel yang dimasukkan.")
 
 def main():
+    # Apply custom CSS for background image
+    add_background()
+
     show_splash_screen()
 
-    # Dictionary to store pH categories, colors, and examples
+    # Dictionary to store pH categories, colors, examples, and definitions
     ph_categories = {
-        "BASA KUAT": (11, 14, "#8A2BE2", "Amonia dan Larutan Alkali", "Basa kuat adalah larutan dengan pH tinggi, yang berarti konsentrasi ion OH- dalam larutan tinggi. Contohnya adalah larutan amonia dan larutan alkali."),
-        "BASA LEMAH": (9, 10, "#800080", "Soda Kue dan Sabun", "Basa lemah adalah larutan dengan pH sedang, yang berarti konsentrasi ion OH- dalam larutan sedang. Contohnya adalah soda kue dan sabun."),
-        "BASA SANGAT LEMAH": (8, 8, "#0000FF", "Sabun", "Basa sangat lemah adalah larutan dengan pH rendah, yang berarti konsentrasi ion OH- dalam larutan rendah. Contohnya adalah sabun."),
-        "NETRAL": (7, 7, "#008000", "Air", "Netral adalah larutan dengan pH netral, yang berarti konsentrasi ion H+ dan OH- dalam larutan seimbang. Contohnya adalah air."),
-        "ASAM SANGAT LEMAH": (6, 6, "#FFFF00", "Susu", "Asam sangat lemah adalah larutan dengan pH rendah, yang berarti konsentrasi ion H+ dalam larutan sedang. Contohnya adalah susu."),
-        "ASAM LEMAH": (4, 5, "#FFA500", "Kopi Hitam, Tomat", "Asam lemah adalah larutan dengan pH rendah, yang berarti konsentrasi ion H+ dalam larutan sedang. Contohnya adalah kopi hitam dan tomat."),
-        "ASAM KUAT": (1, 3, "#FF0000", "Gastric acid, Jeruk, Apel", "Asam kuat adalah larutan dengan pH sangat rendah, yang berarti konsentrasi ion H+ dalam larutan tinggi. Contohnya adalah asam lambung, jeruk, dan apel.")
+        "BASA KUAT": (11, 14, "#8A2BE2", "Natrium Hidroksida (NaOH), Amonia (NH₃)", 
+                      "Basa kuat adalah larutan dengan pH tinggi, yang berarti konsentrasi ion OH- dalam larutan tinggi. "
+                      "Kegunaan Basa Kuat termasuk dalam produk pembersih rumah tangga, "
+                      "pembuatan kertas, sabun, minuman ringan, pengawet makanan, dan obat-obatan."),
+        "BASA LEMAH": (9, 10, "#800080", "Soda Kue dan Sabun", 
+                       "Basa lemah adalah larutan dengan pH sedang, yang berarti konsentrasi ion OH- dalam larutan sedang. "
+                       "Kegunaan Basa Lemah termasuk menyesuaikan atau menetralkan pH, pembuatan pupuk, "
+                       "pemisahan senyawa organik, produksi kertas, dan menyesuaikan pH tanah."),
+        "BASA SANGAT LEMAH": (8, 8, "#0000FF", "Sabun", 
+                              "Basa sangat lemah adalah larutan dengan pH rendah, yang berarti konsentrasi ion OH- dalam larutan rendah. "
+                              "Kegunaan Basa Sangat Lemah termasuk menyesuaikan atau menetralkan pH, pembuatan pupuk, "
+                              "dan produksi kertas."),
+        "NETRAL": (7, 7, "#008000", "Air Murni", 
+                   "Netral adalah larutan dengan pH netral, yang berarti konsentrasi ion H+ dan OH- dalam larutan seimbang. "
+                   "Kegunaan pH netral sebagai air mineral yang diminum sehari-hari oleh manusia."), 
+        "ASAM SANGAT LEMAH": (6, 6, "#FFFF00", "Susu", 
+                              "Asam sangat lemah adalah larutan dengan pH rendah, yang berarti konsentrasi ion H+ dalam larutan sedang. "
+                              "Kegunaan Asam Sangat Lemah termasuk sebagai etchant gigi, aditif makanan, pendispersi, "
+                              "fluks, inhibitor karat, elektrolit, bahan baku pupuk, dan produk pembersih rumah."),
+        "ASAM LEMAH": (4, 5, "#FFA500", "Kopi Hitam, Tomat", 
+                       "Asam lemah adalah larutan dengan pH rendah, yang berarti konsentrasi ion H+ dalam larutan sedang. "
+                       "Kegunaan Asam Lemah termasuk sebagai etchant gigi, aditif makanan, pendispersi, "
+                       "fluks, inhibitor karat, elektrolit, bahan baku pupuk, dan produk pembersih rumah."),
+        "ASAM KUAT": (1, 3, "#FF0000", "Asam Lambung, Jeruk, Apel", 
+                      "Asam kuat adalah larutan dengan pH sangat rendah, yang berarti konsentrasi ion H+ dalam larutan tinggi. "
+                      "Kegunaan Asam Kuat termasuk cairan pengisi aki motor, bahan pupuk kimia, "
+                      "dan pembersih logam dari korosi.")
     }
 
-    nilai = st.number_input('Masukkan nilai pH:', min_value=0.0, max_value=14.0, value=7.0, step=0.1)
+    # Dictionary to map sample names to pH categories
+    sample_to_ph = {
+        "Asam lambung": "ASAM KUAT",
+        "Jeruk": "ASAM KUAT",
+        "Apel": "ASAM KUAT",
+        "Kopi hitam": "ASAM LEMAH",
+        "Tomat": "ASAM LEMAH",
+        "Susu": "ASAM SANGAT LEMAH",
+        "Air": "NETRAL",
+        "Sabun": "BASA SANGAT LEMAH",
+        "Soda kue": "BASA LEMAH",
+        "Natrium Hidroksida": "BASA KUAT",
+        "Amonia": "BASA KUAT",
+        "HCL": "ASAM KUAT",
+        "NaOH": "BASA KUAT", 
+        "NH3": "BASA KUAT",
+        "Jus lemon": "ASAM KUAT",
+        "Asam lemak": "ASAM KUAT",
+        "Cuka": "ASAM KUAT",
+        "Yogurt": "ASAM LEMAH",
+        "Air hujan": "ASAM SANGAT LEMAH",
+        "Telur": "ASAM SANGAT LEMAH",
+        "Bayam": "ASAM SANGAT LEMAH",
+        "Larutan soda": "BASA SANGAT LEMAH",
+        "Buffer fosfat": "BASA SANGAT LEMAH",
+        "Pemutih": "BASA LEMAH",
+        "Soda api": "BASA LEMAH",
+        "Larutan kapur": "BASA LEMAH",
+        "Larutan limbah": "BASA KUAT",
+        "Pembasmi lumpur": "BASA KUAT",
+        "Beton basa": "BASA KUAT",
+        "Air kapur jenuh": "BASA KUAT",
+        "Sabun cuci": "BASA KUAT"
+    }
 
-    ph_category, color, example, definition = get_ph_category(nilai, ph_categories)
-    
-    st.markdown(f"<p style='color:{color}; font-size: 24px;'>Sifat Asam Basa = <strong>{ph_category}</strong></p>", unsafe_allow_html=True)
-    st.markdown(f"<p style='color:{color}; font-size: 18px;'>Contoh = <em>{example}</em></p>", unsafe_allow_html=True)
-    st.write(f"Pengertian: {definition}")
+    sample_name = st.text_input('Masukkan nama sampel:')
 
-def get_ph_category(nilai, categories):
-    for category, (min_val, max_val, color, example, definition) in categories.items():
-        if min_val <= nilai <= max_val:
-            return category, color, example, definition
-    return 'Nilai pH di luar rentang yang valid.', '', '', ''
+    if sample_name in sample_to_ph:
+        ph_category = sample_to_ph[sample_name]
+        min_val, max_val, color, example, definition = ph_categories[ph_category]
+        st.markdown(f"<p style='color:{color}; font-size: 24px;'>Nama Sampel: <strong>{sample_name}</strong></p>", unsafe_allow_html=True)
+        st.markdown(f"<p style='color:{color}; font-size: 24px;'>Sifat Asam Basa: <strong>{ph_category}</strong></p>", unsafe_allow_html=True)
+        st.markdown(f"<p style='color:{color}; font-size: 18px;'>Nilai pH: <strong>{min_val} - {max_val}</strong></p>", unsafe_allow_html=True)
+        st.markdown(f"<p style='color:{color}; font-size: 18px;'>Contoh: <em>{example}</em></p>", unsafe_allow_html=True)
+        st.write(f"Pengertian: {definition}")
+    elif sample_name:
+        st.write("Nama sampel tidak dikenali. Silakan coba lagi.")
+
+def add_background():
+    st.markdown(
+        """
+        <style>
+        .stApp {
+            background: url("https://themeliorist.ca/wp-content/uploads/2020/12/%E2%80%94Pngtree%E2%80%94vector-design-background-of-scientist_1160371.png");
+            background-size: cover;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
 
 if __name__ == "__main__":
     main()
